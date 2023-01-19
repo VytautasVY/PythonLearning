4b49a842f9f7b11d901d1dc15a45739243b06359
VytautasVY <vytautas1uziela@gmail.com>
2023-01-19 18:07:31 +0200

    updated

--- elements_selectors.py
+++ elements_selectors.py
@@ -1,16 +1,22 @@
+#ID selector
 cookies = "hs-eu-confirmation-button"
+SaveMyInfoCheckbox = "label_7_13_1"
+Submit = "gform_submit_button_7"
+
+#XPath selector
 countrylist = "/html/body/main/article/div/div/div/div/div/div/form/div[2]/ul/li[6]/div/div/div[2]/b"
+Country = "/html/body/main/article/div/div/div/div/div/div/form/div[2]/ul/li[6]/div/div/div[3]/div/ul/li[128]"
+
+#Name selector
 FirstName = "input_2"
 LastName = "input_3"
 Email = "input_4"
 PhoneNumber = "input_5"
-Country = "/html/body/main/article/div/div/div/div/div/div/form/div[2]/ul/li[6]/div/div/div[3]/div/ul/li[128]"
 City = "input_7"
 City1 = "input_8"
 Zip = "input_9"
 SkillSummary = "input_11"
 CV = "input_7_12"
 AboutUs = "input_17"
-SaveMyInfoCheckbox = "label_7_13_1"
-Submit = "gform_submit_button_7"
+
 
--- text_variables.py
+++ text_variables.py
@@ -1,3 +1,5 @@
+#ID selector
+
 text_FirstName = "input_7_2"
 text_LastName = "input_7_3"
 text_Email = "input_7_4"
@@ -7,7 +9,9 @@ text_city1 = "input_7_7"
 text_city2 = "input_7_8"
 text_Zip = "input_7_9"
 text_SkillSummary = "input_7_11"
-text_CVUploaded = "/html/body/main/article/div/div/div/div/div/div/form/div[2]/ul/li[12]/div[2]/span"
 text_TechMuge = "input_7_17"
 text_Checkbox = "label_7_13_1"
 text_SubmitApplication = "gform_submit_button_7"
+
+#XPath selector
+text_CVUploaded = "/html/body/main/article/div/div/div/div/div/div/form/div[2]/ul/li[12]/div[2]/span"
